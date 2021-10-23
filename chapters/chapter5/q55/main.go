333bffc807f777c2d02150fae1c1ad0e5b22a334
tenkoh <tenkoh@example.com>
2021-10-24 06:27:54 +0900

    refactor: q55, q56. 解答を見て、自分の思考がいまいちだった部分を修正

--- chapters/chapter5/q55/main.go
+++ chapters/chapter5/q55/main.go
@@ -11,18 +11,13 @@ func Solution(W int, a []int) bool {
 	dp[0][0] = true
 	for i := 0; i < N; i++ {
 		for w := 0; w <= W; w++ {
-			// a[i]を選ばない時
+			// a[i]を使わずにwを作れる時
 			dp[i+1][w] = dp[i][w]
 
-			// a[i]を選ぶ時、その前にa[i]を選んだ時とa[i-1]を選んだ時の二通りがある
+			// なんどでもa[i]を足せるので次のように更新
+			// dp[i]からの遷移は　↑ で網羅済み
 			if w-a[i] >= 0 {
 				dp[i+1][w] = dp[i+1][w] || dp[i+1][w-a[i]]
-			}
-			if i < 1 {
-				continue
-			}
-			if w-a[i-1] >= 0 {
-				dp[i+1][w] = dp[i+1][w] || dp[i][w-a[i-1]]
 			}
 		}
 	}
--- chapters/chapter5/q56/main.go
+++ chapters/chapter5/q56/main.go
@@ -15,20 +15,12 @@ func Solution(W int, a, m []int) bool {
 	dp[0][0] = 0
 	for i := 0; i < N; i++ {
 		for w := 0; w <= W; w++ {
-			// a[i]を足さない時、dp[i][w]==InfならInf, それ以外なら0を入れる
-			if dp[i][w] != Inf {
+			// a[i]を足さずにwが作れる時==dp[i][w]<Infならdp[i+1][w]=0
+			if dp[i][w] < Inf {
 				dp[i+1][w] = 0
 			}
-			// a[i]を足す時
-			// dp[i-1]からきた時とdp[i]からきた時で分ける
-			// 遷移してこられるかの条件は、遷移前の加算回数がm未満のこと
-			if i > 0 {
-				if w-a[i-1] >= 0 {
-					if dp[i][w-a[i-1]] < m[i-1] {
-						util.Chmin(&dp[i+1][w], 1)
-					}
-				}
-			}
+			// dp[i+1][w] < m[i]ならa[i]を加算できる
+			// 到達不可能な組み合わせ=Infなので、同じ処理でスキップできる
 			if w-a[i] >= 0 {
 				if pre := dp[i+1][w-a[i]]; pre < m[i] {
 					util.Chmin(&dp[i+1][w], pre+1)
